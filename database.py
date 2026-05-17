c1f5f05b23b7ec7a7e058d4116f805e10a8b309e
yagizalpzeybek <yagizalpzeybek20@gmail.com>
2026-05-17 21:48:36 +0300

    Update

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
 
+
+print(repr(SUPABASE_URL))
+print(repr(SUPABASE_KEY))
+
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
